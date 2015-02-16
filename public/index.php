6c44fbff551c259292a4234b83f59d53e3b75050
Sasan Rose <sasan.rose@gmail.com>
2015-02-16 23:18:58 +0330

    Separate autoload from bootstrap file

--- public/index.php
+++ public/index.php
@@ -1,22 +1,6 @@
 <?php
 
-function __autoload($class)
-{
-    $path = '../';
-    if (preg_match('/^(.*)_Controller$/', $class, $matches)) {
-        $class = $matches[1];
-        $dir   = 'controllers';
-    } elseif (preg_match('/^(.*)_Models$/', $class, $matches)) {
-        $class = $matches[2];
-        $dir   = 'models';
-    } elseif (preg_match('/^(.*)_Helpers$/', $class, $matches)) {
-        $class = $matches[2];
-        $dir   = 'helpers';
-    } else
-        $dir = 'libraries';
-
-    include_once($path.$dir.'/'.(strtolower($class)).'.php');
-}
+require_once("../libraries/autoload.php");
 
 if (isset(App::instance()->config['timezone']))
     date_default_timezone_set(App::instance()->config['timezone']);
