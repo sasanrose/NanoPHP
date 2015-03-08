d77b0f876b4bd277ed6c4bd535bb2ad28c953b91
Sasan Rose <sasan.rose@gmail.com>
2015-03-08 10:38:25 +0330

    Rewriting the whole framework

--- controllers/welcome.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Welcome_Controller extends Controller
-{
-    public function indexAction()
-    {
-        Template::factory()->render('welcome/index');
-    }
-}
--- views/welcome/index.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo "Welcome to NanoPHP";
